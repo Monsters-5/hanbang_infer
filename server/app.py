ff2dc862d566ab722cc3d14a571e3af02126db5b
KimJeongHo <76683835+Jeongho-K@users.noreply.github.com>
2023-11-10 15:36:03 +0900

    add first status

--- server/app.py
+++ server/app.py
@@ -14,9 +14,13 @@ HOME = os.getcwd()
 MODEL = f"{HOME}/server/weights/best.pt"
 model = YOLO(MODEL)
 
+@app.route('/')
+def status():
+    return 'connected'
+
 # if a request POST is made on this url it runs the running function below
 # CCTV영상 FPS는 30이라고 가정, 3초동안 측정된 개수의 중앙값과 timestamp return
-@app.route('/predict',methods=['POST']) 
+@app.route('/predict',methods=['POST'])
 def running():
     # start infer by yolo
     response = start_infer(model)
